35a4781d17b0c55727ee20684f6c4da4358edf17
Eity2021 <Eity2021akter@gmail.com>
2025-03-22 19:33:00 +0600

    server

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const serverless = require("serverless-http");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-
-// Import routes
 const authRouter = require("./routes/auth/auth-routes");
+const dotenv = require("dotenv");
 const productRouter = require("./routes/product/productRoute");
 const categoriesRouter = require("./routes/categories/categoriesRoute");
 const subCategoriesRouter = require("./routes/subCategories/subCategoriesRoute");
@@ -20,39 +18,47 @@ const couponRouter = require("./routes/coupon/couponRoute");
 const middleBannerRouter = require("./routes/banner/middleBannerRoute");
 
 dotenv.config();
-
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected Successfully"))
-  .catch((error) => console.log("MongoDB Error:", error));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    Credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
+app.use("/api/adminAuth", adminRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/subCategories", subCategoriesRouter);
+app.use("/api/productCart", cartRouter);
+app.use("/api/banner", bannerRouter);
+app.use("/api/middle", middleBannerRouter);
+app.use("/api/wish", wishListRouter);
+app.use("/api/checkedOut", checkoutRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/coupon", couponRouter);
 
-
-const router = express.Router();
-router.use("/adminAuth", adminRouter);
-router.use("/auth", authRouter);
-router.use("/product", productRouter);
-router.use("/categories", categoriesRouter);
-router.use("/subCategories", subCategoriesRouter);
-router.use("/productCart", cartRouter);
-router.use("/banner", bannerRouter);
-router.use("/middle", middleBannerRouter);
-router.use("/wish", wishListRouter);
-router.use("/checkedOut", checkoutRouter);
-router.use("/order", orderRouter);
-router.use("/coupon", couponRouter);
-
-app.use("/api", router); 
-
-// Test API Route
 app.get("/", (req, res) => {
-  res.send("API is working!");
+  res.send("api working");
 });
-
-// ✅ Correct serverless export for Vercel
 module.exports = serverless(app);
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
